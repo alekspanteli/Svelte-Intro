dc7d52b7c0f5c40b4a8733bca8d2a04f36abfe79
alekspanteli <alekspanteli@rambler.ru>
2022-08-08 15:16:47 +0300

    Svelte testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,33 @@
 <script>
-	let name = 'world';
-  let src = './assets/svelte.svg';
+import Nested from './Nested.svelte';
+	// let name = 'world';
+  // let src = 'https://svelte.dev/tutorial/image.gif';
+  // let string = 'this string contains some <strong>HTML!!!</strong>';
+  let count = 0;
+
+	function incrementCount() {
+	count += 1;
+	}
+  
 </script>
 
-<h1>Hello {name.toUpperCase()}!</h1>
+<style>
+  /* p {
+      color: purple;
+      font-family: 'Comic Sans MS', cursive;
+      font-size: 2em;
+    } */
+</style>
 
-<img src={src} alt="Image goes here">
+<!-- <h1>Hello {name.toUpperCase()}!</h1> -->
+
+<!-- <img src={src} alt="Image goes here">
+
+<p>This is a paragraph.</p>
+<p>{@html string}</p> -->
+
+Clicked {count} {count === 1 ? 'time' : 'times'}
+<button on:click={incrementCount}>Count</button>
+<Nested/>
+
+
