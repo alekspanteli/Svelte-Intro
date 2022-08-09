29ecd48d9cf2da2afd9a58d2e006bf991254c053
alekspanteli <alekspanteli@rambler.ru>
2022-08-09 11:03:30 +0300

    add each block

--- src/App.svelte
+++ src/App.svelte
@@ -17,9 +17,9 @@ let cats = [
 <h1>The Famous Cats of YouTube</h1>
 
 <ul>
-	{#each cats as cat}
-		<li><a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
-			{cat.name}
+	{#each cats as { id, name }, i}
+		<li><a target="_blank" href="https://www.youtube.com/watch?v={id}">
+			{i + 1}: {name}
 		</a></li>
 	{/each}
 </ul>
